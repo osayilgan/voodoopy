c2a90a3c020ea687177f93135e77e6af713752e6
yusufkaratoprak <yusufkaratoprak@gmail.com>
2024-11-03 09:38:06 +0100

    update for setup

--- setup.py
+++ setup.py
@@ -7,13 +7,19 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="yusuf karatoprak",
-    author_email="yusuf.karatoprak@voodoopy.com",
+    author_email="yusuf.karatoprak@gmail.com",
+    url = "https://github.com/yusufkaratoprak/voodoopy",
     packages=find_packages(),  # Automatically find packages in the directory
     include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
     install_requires=[
         "Flask",
         "requests",
         "flask_cors"
     ],
-    python_requires=">=3.6",
-)
+    )
