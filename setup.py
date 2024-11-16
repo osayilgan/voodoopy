4e912deb20bb5aeb41a19567e2f412d944f84f1d
yusufkaratoprak <yusufkaratoprak@gmail.com>
2024-11-16 12:47:23 +0100

    update for versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="voodoo-gui",
-    version="0.1",
+    version="0.1.1",
     description="A Python library to create UIs like Streamlit but with simple syntax",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
